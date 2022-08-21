5686c3c0c237ad398d8ffbbfc5bc8a10bae1a45d
Yeasaleh <yea.saleh@Mimothi.com>
2022-08-21 23:12:51 +0600

    fast

---
+++ Fast.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main ()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<"A is greater"<<endl;
+    }
+    else if(b>a)
+    {
+        cout<<"B is greater"<<endl;
+    }
+    else
+    {
+        cout<<"A and b equal"<<endl;
+    }
+    return 0 ;
+
+}
+
